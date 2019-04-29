5d96c7a28ed75e03e3b57628c522f7101d6b960f
Zizhao Wang <princez.wzz@gmail.com>
2019-04-28 21:57:32 -0400

    remove jsx-tag-spacing rule that conflict with prettier

--- index.js
+++ index.js
@@ -5,7 +5,10 @@ module.exports = {
 		'plugin:@typescript-eslint/recommended',
 		'prettier/@typescript-eslint',  // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
     'plugin:prettier/recommended',  // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
-	],
+  ],
+  plugin: [
+    'prettier'
+  ],
 	parserOptions: {
 		ecmaVersion: 2018,  // Allows for the parsing of modern ECMAScript features
 		sourceType: 'module',  // Allows for the use of imports
@@ -52,12 +55,6 @@ module.exports = {
 		'react/jsx-key': ['error'],
 		'react/jsx-no-bind': ['warn', { 'allowArrowFunctions': true }],
 		'react/self-closing-comp': ['error'],
-		'react/jsx-tag-spacing': ['error', {
-			'closingSlash': 'never',
-			'beforeSelfClosing': 'always',
-			'afterOpening': 'never',
-			'beforeClosing': 'never'
-		}],
 		'react/no-access-state-in-setstate': ['warn'],
 		'react/no-array-index-key': 'warn',
   },
